d75977b55487ffdc97374b543a5fddbd67b39cb7
wroyce.misa <wroyce.misa@ibm.com>
2024-03-14 13:08:31 +0800

    update in js activity

--- activity-array/assets/array.js
+++ activity-array/assets/array.js
@@ -1,13 +1,17 @@
 let Items  = [];
-
 document.getElementById("btn-log").addEventListener('click', function() {
     let EmployeeID = document.getElementById("txtEmployeeID").value;
     let EmployeeName = document.getElementById("txtEmployeeName").value;
     let WorkingHours = document.getElementById("txtWorkingHours").value;
-    console.log("EmployeeID: " +EmployeeID + " EmployeeName: "+ EmployeeName + " WorkingHours: "+ WorkingHours);
+    console.log("Employee ID: " +EmployeeID + " Employee Name: "+ EmployeeName + " Working Hours: "+ WorkingHours);
 
+    if(EmployeeID=="" && EmployeeName=="" && WorkingHours ==""){
+        alert("Employeed ID and Employee Name and Working Hours are required fields");
+
+    }else{
     Items.push( [EmployeeID, EmployeeName, WorkingHours] );
     DrawList();
+}
 });
 
 function remove(index) {
@@ -19,7 +23,6 @@ function DrawList() {
     let List = "";
     console.log("Items length: "+ Items.length);
     for(let x = 0; x < Items.length; x++)
-        List += "<li class='mt-1'>"+ Items[x][0] + " | " + Items[x][1] + " | " + Items[x][2] + "<button class='btn btn-danger' onclick='remove("+x+")'>Remove</button></li>";
-
-    document.getElementById("ul-list").innerHTML = List;
+        List += "<tr></tr><td>"+ Items[x][0]+" </td><td> "+ Items[x][1]+" </td><td>"+ Items[x][2]+"</td><td><button class='btn btn-danger' onclick='remove("+x+")'>Remove</button></td>";
+    document.getElementById("tableWorkers").innerHTML = "<table id='workers'><td colspan='4'><h5>List of Workers</h5></td><tr></tr><td>ID</td><td>Name</td><td>Hours</td><td>ACTION</td>"+List+"</table>";
 }
