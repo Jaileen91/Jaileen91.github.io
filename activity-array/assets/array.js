73b1a383f5a84e9cb5eb5c345dec035033e9eac7
wroyce.misa <wroyce.misa@ibm.com>
2024-03-14 13:33:05 +0800

    add total

--- activity-array/assets/array.js
+++ activity-array/assets/array.js
@@ -15,14 +15,18 @@ document.getElementById("btn-log").addEventListener('click', function() {
 });
 
 function remove(index) {
-    Items.splice(index, 2);
+    Items.splice(index, 1);
     DrawList();
 }
 
 function DrawList() {
     let List = "";
+    let total = 0
     console.log("Items length: "+ Items.length);
+    for(let x = 0; x < Items.length; x++){
+        total = parseInt(total)+ parseInt(Items[x][2]);
+    }
     for(let x = 0; x < Items.length; x++)
         List += "<tr></tr><td>"+ Items[x][0]+" </td><td> "+ Items[x][1]+" </td><td>"+ Items[x][2]+"</td><td><button class='btn btn-danger' onclick='remove("+x+")'>Remove</button></td>";
-    document.getElementById("tableWorkers").innerHTML = "<table id='workers'><td colspan='4'><h5>List of Workers</h5></td><tr></tr><td>ID</td><td>Name</td><td>Hours</td><td>ACTION</td>"+List+"</table>";
+    document.getElementById("tableWorkers").innerHTML = "<table id='workers'><td colspan='4'><h5>List of Workers</h5></td><tr></tr><td>ID</td><td>Name</td><td>Hours</td><td>ACTION</td>"+List+"<tr></tr><td>TOTAL<td><td colspan='2'>"+total+"</td></table>";
 }
